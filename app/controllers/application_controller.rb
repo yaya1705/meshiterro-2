fb55bccb2874294b13591ddbec630c7fcdad8634
yaya1705 <yaya0517k@gmail.com>
2022-08-27 09:12:18 +0000

    deviseを使用した実装4~7章

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
+  # configure_permitted_parametersメソッドが実行され
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ログイン後の遷移先、after_sign_in_path_forはDeviseが用意しているメソッド
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
+  # サインアウト後の遷移先、after_sign_out_path_forはDeviseが用意しているメソッド
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+
+  # ストロングパラメータと同様の機能
+    # privateは記述をしたコントローラ内でしか参照できません
+    # 一方、protectedは呼び出された他のコントローラからも参照することができる。
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+  end
 end
+
+
