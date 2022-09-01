f4c7a19ec56dde2e3a66e1ad1c39ef3afc95a898
yaya1705 <yaya0517k@gmail.com>
2022-09-01 13:07:49 +0000

    18章-コメント機能

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,8 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   
+  has_many :post_comments, dependent: :destroy
+  
 # 画像が設定されない場合はapp/assets/imagesに格納されている
 # no_image.jpgという画像をデフォルト画像としてActiveStorageに格納し、格納した画像を表示する
   def get_image
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
-  resources :post_images, only:[:index, :new, :create, :show, :destroy]
+  resources :post_images, only:[:index, :new, :create, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only:[:edit, :update, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
